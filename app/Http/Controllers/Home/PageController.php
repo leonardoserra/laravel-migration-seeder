c9e12764c026f3ded9c9cfbb439aa66fc1f7554d
leonardoserra <serra.leonardo92@gmail.com>
2023-05-15 21:00:24 +0200

    Creato seeder con nome appropriato

--- app/Http/Controllers/Home/PageController.php
+++ app/Http/Controllers/Home/PageController.php
@@ -8,8 +8,8 @@ use App\Models\Train;
 class PageController extends Controller
 {
     public function index(){
-        $trains = Train::whereDate('data_di_partenza', '=', date('Y-m-d'))->get();
-        // $trains = Train::all();
+        // $trains = Train::whereDate('data_di_partenza', '=', date('Y-m-d'))->get();
+        $trains = Train::all();
         // dd($trains);
         return view('home', compact('trains'));
     }
--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -6,9 +6,9 @@ use App\Functions\Helpers;
 use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Faker\Generator as Faker;
+// use Faker\Generator as Faker;
 
-class UserTableSeeder extends Seeder{
+class TrainsTableSeeder extends Seeder{
     /**
      * Run the database seeds.
      *
